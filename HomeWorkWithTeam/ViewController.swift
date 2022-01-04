5dcfdd8a97bba3a0e3a99d2ea43d723219a6671a
Dmitry <radiator211@mail.ru>
2022-01-04 11:42:22 +0700

    Initial Commit

---
+++ HomeWorkWithTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkWithTeam
+//
+//  Created by Дмитрий Бессонов on 04.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
